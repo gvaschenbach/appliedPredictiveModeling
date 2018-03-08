396d9623f10f45a6701698ca09427c19002e9583
Peter Bjorn Jones <gvaschenbach@gmailcom>
2018-03-08 12:55:44 -0700

    added chapter 1 exersises

--- munge/01-A.R
+++ munge/01-A.R
@@ -1,1 +1,14 @@
 # Example preprocessing script.
+library(AppliedPredictiveModeling)
+data( "segmentationOriginal" )
+
+# Split into train set
+segdata <- subset( segmentationOriginal, Case == 'Train' )
+
+# Separate out cell and class variables
+cellID <- segdata$Cell
+class <- segdata$Class
+case <- segdata$Case
+
+# Remove columns
+segdata <- segdata[, -(1:3)]
